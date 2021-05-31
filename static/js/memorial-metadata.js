6f4363e9f5ab1c4695b9deefe1cf8933594b9d5e
DJFriar <tommy.craft@icloud.com>
2021-05-30 22:00:54 -0500

    Stubbed out all API calls, added DataTables

---
+++ static/js/memorial-metadata.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('#memorialMetadataTable').DataTable();
+  $('#restrictionsTable').DataTable();
+  $('#categoryTable').DataTable();
+
+});
--- static/js/pending-memorials.js
+++
@@ -1,6 +0,0 @@
-  // Handle add new memorial toggle
-  $(".addMemorialButton").on("click", function(event) {
-    event.preventDefault();
-    $("#editMemorialInfo").toggleClass("hide-me");
-    $(".addMemorialBtnDiv").toggleClass("hide-me");
-  });
