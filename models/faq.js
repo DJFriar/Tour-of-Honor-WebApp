71990d0810fa3851921b634fc60da2d6d1dc0fd7
Tommy Craft <tommy.craft@icloud.com>
2022-10-16 16:02:08 -0500

    Added FAQ page

---
+++ models/faq.js
@@ -0,0 +1,14 @@
+module.exports = function(sequelize, DataTypes) {
+  const Faq = sequelize.define("Faq", {
+    Question: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    Answer: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  return Faq;
+};
