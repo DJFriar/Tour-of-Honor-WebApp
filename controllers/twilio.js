981c42d5ea5bb52e3f177158f122f4f404d8a761
Tommy Craft <tommy.craft@icloud.com>
2022-10-25 02:20:33 -0500

    code linting

--- controllers/twilio.js
+++ controllers/twilio.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
-const twilio = require('twilio');
+// const twilio = require('twilio');
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const fromNumber = process.env.TWILIO_FROM_NUMBER;
+
 const client = require('twilio')(accountSid, authToken);
 
 function sendSMSMessage(destNumber, messageText) {
-  const formattedNumber = "+1" + destNumber.replace(/^(\+)|\D/g, "$1");
+  const formattedNumber = `+1${destNumber.replace(/^(\+)|\D/g, '$1')}`;
 
   const smsMessage = {
     to: formattedNumber,
@@ -14,12 +15,10 @@ function sendSMSMessage(destNumber, messageText) {
     body: messageText,
   };
 
-  client.messages
-  .create(smsMessage)
-  .then((message) => {
+  client.messages.create(smsMessage).then((message) => {
     console.log(`Message SID ${message.sid}`);
     return message.sid;
   });
-};
+}
 
 exports.sendSMSMessage = sendSMSMessage;
