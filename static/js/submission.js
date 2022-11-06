ed9dce7777b2f24f6dffaf6aec941c27c50636ad
Tommy Craft <tommy.craft@icloud.com>
2022-11-06 17:24:23 -0600

    Code Format cleanup

--- static/js/submission.js
+++ static/js/submission.js
@@ -1,113 +1,105 @@
-$(document).ready(function () {
-  var selectedFilter = "All";
-  selectedFilter = localStorage.getItem("categoryFilter");
-  $("#selectFilterValue").text(selectedFilter);
+$(document).ready(() => {
+  let selectedFilter = 'All';
+  selectedFilter = localStorage.getItem('categoryFilter');
+  $('#selectFilterValue').text(selectedFilter);
 
   // Validate Scorer Notes before allowing Rejection
-  $("#scorerNotes").on("input", function () { 
-    var scorerNotes = $("#scorerNotes").val().trim()
+  $('#scorerNotes').on('input', () => {
+    const scorerNotes = $('#scorerNotes').val().trim();
     if (scorerNotes.length >= 5) {
-      $(".rejectButton").removeAttr("disabled");
-      $("#rejectButtonSpan").removeAttr("uk-tooltip");
+      $('.rejectButton').removeAttr('disabled');
+      $('#rejectButtonSpan').removeAttr('uk-tooltip');
     } else {
-      $(".rejectButton").attr("disabled", "disabled");      
+      $('.rejectButton').attr('disabled', 'disabled');
     }
   });
 
   // Handle Approve Button
-  $(".approveButton").on("click", function() {
-    var subID = $(this).data("submissionid");
-    var scorerNotes = $("#scorerNotes").val().trim();
-    var submitterID = $("#SubmissionUserID").val();
-    var submittedMemorialID = $("#SubmissionMemorialID").val();
-    var submittedFlagNumber = $("#SubmissionFlagNum").val();
-    var submittedOtherRiders = $("#SubmissionOtherRiders").val();
-    var submissionInfo = {
+  $('.approveButton').on('click', function () {
+    const subID = $(this).data('submissionid');
+    const scorerNotes = $('#scorerNotes').val().trim();
+    const submitterID = $('#SubmissionUserID').val();
+    const submittedMemorialID = $('#SubmissionMemorialID').val();
+    const submittedFlagNumber = $('#SubmissionFlagNum').val();
+    const submittedOtherRiders = $('#SubmissionOtherRiders').val();
+    const submissionInfo = {
       SubmissionID: subID,
       ScorerNotes: scorerNotes,
       SubmittedMemorialID: submittedMemorialID,
       SubmittedUserID: submitterID,
       SubmittedFlagNumber: submittedFlagNumber,
       SubmittedOtherRiders: submittedOtherRiders,
-      Status: 1
+      Status: 1,
     };
 
-    $.ajax("/handle-submission", {
-      type: "PUT",
-      data: submissionInfo
-    }).then(
-      goToNextPendingSubmission(selectedFilter)
-    );
+    $.ajax('/handle-submission', {
+      type: 'PUT',
+      data: submissionInfo,
+    }).then(goToNextPendingSubmission(selectedFilter));
   });
 
   // Handle Skip Button
-  $(".skipButton").on("click", function() {
-    var subID = $(this).data("submissionid");
-    var scorerNotes = $("#scorerNotes").val().trim()
-    var submissionInfo = {
+  $('.skipButton').on('click', function () {
+    const subID = $(this).data('submissionid');
+    const scorerNotes = $('#scorerNotes').val().trim();
+    const submissionInfo = {
       SubmissionID: subID,
       ScorerNotes: scorerNotes,
-      Status: 3
+      Status: 3,
     };
 
-    $.ajax("/handle-submission", {
-      type: "PUT",
-      data: submissionInfo
-    }).then(
-      goToNextPendingSubmission(selectedFilter)
-    );
-  })
+    $.ajax('/handle-submission', {
+      type: 'PUT',
+      data: submissionInfo,
+    }).then(goToNextPendingSubmission(selectedFilter));
+  });
 
   // Handle Reject Button
-  $(".rejectButton").on("click", function() {
-    var subID = $(this).data("submissionid");
-    var scorerNotes = $("#scorerNotes").val().trim()
-    var submissionInfo = {
+  $('.rejectButton').on('click', function () {
+    const subID = $(this).data('submissionid');
+    const scorerNotes = $('#scorerNotes').val().trim();
+    const submissionInfo = {
       SubmissionID: subID,
       ScorerNotes: scorerNotes,
-      Status: 2
+      Status: 2,
     };
 
-    $.ajax("/handle-submission", {
-      type: "PUT",
-      data: submissionInfo
-    }).then(
-      goToNextPendingSubmission(selectedFilter)
-    );
+    $.ajax('/handle-submission', {
+      type: 'PUT',
+      data: submissionInfo,
+    }).then(goToNextPendingSubmission(selectedFilter));
   });
 
   // Handle POTM Button
-  $(".potmButton").on("click", function() {
-    var subID = $(this).data("submissionid");
-    var potmInfo = {
-      SubmissionID: subID
+  $('.potmButton').on('click', function () {
+    const subID = $(this).data('submissionid');
+    const potmInfo = {
+      SubmissionID: subID,
     };
 
-    $.ajax("/handle-potmSubmission", {
-      type: "PUT",
-      data: potmInfo
-    })
+    $.ajax('/api/v1/email/potmSubmission', {
+      type: 'PUT',
+      data: potmInfo,
+    });
   });
 
   function goToNextPendingSubmission(category) {
-    $.ajax("/api/v1/submission/" + category, {
-      type: "GET"
-    }).then(
-      function(res) { 
-        console.log("Going to submission " + res[0].id)
-        location.assign("/submission/" + res[0].id); 
-      }
-    )
+    $.ajax(`/api/v1/submission/${category}`, {
+      type: 'GET',
+    }).then((res) => {
+      console.log(`Going to submission ${res[0].id}`);
+      location.assign(`/submission/${res[0].id}`);
+    });
   }
 
   // function skipPendingSubmission(category, subID) {
   //   $.ajax("/api/v1/skipsubmission/" + category + "/" + subID, {
   //     type: "GET"
   //   }).then(
-  //     function(res) { 
+  //     function(res) {
   //       console.log("Skipped " + subID + ". Going to submission " + res[0].id)
-  //       location.assign("/submission/" + res[0].id); 
+  //       location.assign("/submission/" + res[0].id);
   //     }
   //   )
   // }
-})
+});
