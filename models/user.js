f78e89a09abbd3074db0f3d655a0ce4e6c1ae409
Tommy Craft <tommy.craft@icloud.com>
2022-11-07 23:54:27 -0600

    removed FlagNumber from User table

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable func-names */
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 
-module.exports = function(sequelize, DataTypes) {
-  const User = sequelize.define("User", {
+module.exports = function (sequelize, DataTypes) {
+  const User = sequelize.define('User', {
     FirstName: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -14,69 +16,61 @@ module.exports = function(sequelize, DataTypes) {
     UserName: {
       type: DataTypes.STRING,
       allowNull: true,
-      unique: true
+      unique: true,
     },
     Email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: {
-        isEmail: true
-      }
+        isEmail: true,
+      },
     },
     Password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     Address1: {
       type: DataTypes.STRING,
     },
     City: {
-      type:DataTypes.STRING,
+      type: DataTypes.STRING,
     },
     State: {
-      type:DataTypes.STRING,
+      type: DataTypes.STRING,
     },
     ZipCode: {
-      type:DataTypes.STRING,
+      type: DataTypes.STRING,
     },
     CellNumber: {
-      type:DataTypes.STRING,
+      type: DataTypes.STRING,
     },
     TimeZone: {
       type: DataTypes.STRING,
     },
     ProfilePic: {
-      type: DataTypes.STRING
-    },
-    FlagNumber: {
-      type: DataTypes.INTEGER,
-      allowNull: false
+      type: DataTypes.STRING,
     },
     PillionFlagNumber: {
       type: DataTypes.INTEGER,
     },
     isAdmin: {
       type: DataTypes.BOOLEAN,
-      defaultValue: 0
+      defaultValue: 0,
     },
     isActive: {
       type: DataTypes.BOOLEAN,
-      defaultValue: 0
-    }
+      defaultValue: 0,
+    },
   });
 
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
+  User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.Password);
   };
   // Before a rider is created, we will automatically hash their password
-  User.addHook("beforeCreate", user => {
-    user.Password = bcrypt.hashSync(
-      user.Password,
-      bcrypt.genSaltSync(10),
-      null
-    );
+  User.addHook('beforeCreate', (user) => {
+    user.Password = bcrypt.hashSync(user.Password, bcrypt.genSaltSync(10), null);
   });
 
   return User;
