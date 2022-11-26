0fe3b5a98c7a1f0290a8954eb130c5e85280be93
Tommy Craft <38347315+DJFriar@users.noreply.github.com>
2022-11-26 16:40:10 -0600

    Fixes a buf on passenger flag selection

--- private/routes/web/fe-routes.js
+++ private/routes/web/fe-routes.js
@@ -788,19 +788,18 @@ module.exports = function (app) {
     }
 
     try {
-      OrderInfoArray = await q.queryOrderInfoByRider(req.user.id, 2023);
+      OrderInfoArray = await q.queryOrderInfoByRider(req.user.id, currentRallyYear);
       OrderInfo = OrderInfoArray[0];
-      console.log('==== OrderInfo ====');
-      console.log(OrderInfo);
+      // console.log('==== OrderInfo ====');
+      // console.log(OrderInfo);
       // Check if Passenger has an existing flag number.
       if (OrderInfo.PassUserID && OrderInfo.PassUserID > 0) {
         try {
           const passFlagNum = await q.queryFlagNumFromUserID(OrderInfo.PassUserID, 2022);
+          console.log(passFlagNum);
           if (passFlagNum && passFlagNum.FlagNumber > 0) {
-            OrderInfo.dataValues.PassFlagNum = passFlagNum.FlagNumber;
             OrderInfo.PassFlagNum = passFlagNum.FlagNumber;
           } else {
-            OrderInfo.dataValues.PassFlagNum = 0;
             OrderInfo.PassFlagNum = 0;
           }
         } catch (err) {
