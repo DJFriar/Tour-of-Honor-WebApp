5f25feaf2766c926a9ddb9901bdc1847a3f6849d
Tommy Craft <38347315+DJFriar@users.noreply.github.com>
2022-11-29 02:05:15 -0600

    Adds test version of new Scored page, fixes old one

--- static/js/scored.js
+++ static/js/scored.js
@@ -1,104 +1,157 @@
-$(document).ready(function () {
+$(document).ready(() => {
   // Create the Scored Table
-  var scoredTable = $('#ScoredTable').DataTable({
-    "order": [[ 1, "desc" ]],
-    "pageLength": 50,
-    "columnDefs": [
+  const scoredTable = $('#ScoredTable').DataTable({
+    order: [[1, 'desc']],
+    pageLength: 50,
+    columnDefs: [
       {
-        "targets": [0],
-        "visible": false,
-        "Searchable": false
+        targets: [0],
+        visible: false,
+        Searchable: false,
       },
       {
-        "targets": [1],
-        "visible": false,
-        "Searchable": false
-      }
-    ]
+        targets: [1],
+        visible: false,
+        Searchable: false,
+      },
+    ],
   });
-
-  var scoredTableData  = scoredTable.rows().data();
 
   // Force tables to be full width
   $('table#ScoredTable').css('width', '100%');
-  
+
   // Handle Filter Buttons on Pending Table
-  localStorage.setItem("categoryFilter","All");
+  localStorage.setItem('categoryFilter', 'All');
 
   $('.showAllSubmissions').on('click', () => {
-    $(".submissionFilterButton").prevAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".submissionFilterButton").nextAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".showAllSubmissions").removeClass("uk-button-secondary").addClass("uk-button-primary");
-    setSubmissionFilter("All");
-    
-  })
+    $('.submissionFilterButton')
+      .prevAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.submissionFilterButton')
+      .nextAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.showAllSubmissions').removeClass('uk-button-secondary').addClass('uk-button-primary');
+    setSubmissionFilter('All');
+  });
   $('.showTOHSubmissions').on('click', () => {
-    $(".submissionFilterButton").prevAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".submissionFilterButton").nextAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".showTOHSubmissions").removeClass("uk-button-secondary").addClass("uk-button-primary");
-    setSubmissionFilter("Tour of Honor");
-  })
+    $('.submissionFilterButton')
+      .prevAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.submissionFilterButton')
+      .nextAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.showTOHSubmissions').removeClass('uk-button-secondary').addClass('uk-button-primary');
+    setSubmissionFilter('Tour of Honor');
+  });
   $('.show911Submissions').on('click', () => {
-    $(".submissionFilterButton").prevAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".submissionFilterButton").nextAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".show911Submissions").removeClass("uk-button-secondary").addClass("uk-button-primary");
-    setSubmissionFilter("9/11");
-  })
+    $('.submissionFilterButton')
+      .prevAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.submissionFilterButton')
+      .nextAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.show911Submissions').removeClass('uk-button-secondary').addClass('uk-button-primary');
+    setSubmissionFilter('9/11');
+  });
   $('.showDBSubmissions').on('click', () => {
-    $(".submissionFilterButton").prevAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".submissionFilterButton").nextAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".showDBSubmissions").removeClass("uk-button-secondary").addClass("uk-button-primary");
-    setSubmissionFilter("Doughboys");
-  })
+    $('.submissionFilterButton')
+      .prevAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.submissionFilterButton')
+      .nextAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.showDBSubmissions').removeClass('uk-button-secondary').addClass('uk-button-primary');
+    setSubmissionFilter('Doughboys');
+  });
   $('.showGSFSubmissions').on('click', () => {
-    $(".submissionFilterButton").prevAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".submissionFilterButton").nextAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".showGSFSubmissions").removeClass("uk-button-secondary").addClass("uk-button-primary");
-    setSubmissionFilter("Gold Star Family");
-  })
+    $('.submissionFilterButton')
+      .prevAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.submissionFilterButton')
+      .nextAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.showGSFSubmissions').removeClass('uk-button-secondary').addClass('uk-button-primary');
+    setSubmissionFilter('Gold Star Family');
+  });
   $('.showK9Submissions').on('click', () => {
-    $(".submissionFilterButton").prevAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".submissionFilterButton").nextAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".showK9Submissions").removeClass("uk-button-secondary").addClass("uk-button-primary");
-    setSubmissionFilter("War Dogs / K9");
-  })
+    $('.submissionFilterButton')
+      .prevAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.submissionFilterButton')
+      .nextAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.showK9Submissions').removeClass('uk-button-secondary').addClass('uk-button-primary');
+    setSubmissionFilter('War Dogs / K9');
+  });
   $('.showMadonnaSubmissions').on('click', () => {
-    $(".submissionFilterButton").prevAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".submissionFilterButton").nextAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".showMadonnaSubmissions").removeClass("uk-button-secondary").addClass("uk-button-primary");
-    setSubmissionFilter("Madonna Trail");
-  })
+    $('.submissionFilterButton')
+      .prevAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.submissionFilterButton')
+      .nextAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.showMadonnaSubmissions').removeClass('uk-button-secondary').addClass('uk-button-primary');
+    setSubmissionFilter('Madonna Trail');
+  });
   $('.showHueySubmissions').on('click', () => {
-    $(".submissionFilterButton").prevAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".submissionFilterButton").nextAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".showHueySubmissions").removeClass("uk-button-secondary").addClass("uk-button-primary");
-    setSubmissionFilter("Hueys");
-  })
+    $('.submissionFilterButton')
+      .prevAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.submissionFilterButton')
+      .nextAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.showHueySubmissions').removeClass('uk-button-secondary').addClass('uk-button-primary');
+    setSubmissionFilter('Hueys');
+  });
   $('.showSOLMemorials').on('click', () => {
-    $(".MemorialFilterButton").prevAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".MemorialFilterButton").nextAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".showSOLMemorials").removeClass("uk-button-secondary").addClass("uk-button-primary");
-    setSubmissionFilter("Statue of Liberty");
-  })
+    $('.MemorialFilterButton')
+      .prevAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.MemorialFilterButton')
+      .nextAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.showSOLMemorials').removeClass('uk-button-secondary').addClass('uk-button-primary');
+    setSubmissionFilter('Statue of Liberty');
+  });
   $('.showCemeteryMemorials').on('click', () => {
-    $(".MemorialFilterButton").prevAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".MemorialFilterButton").nextAll().removeClass("uk-button-primary").addClass("uk-button-secondary");
-    $(".showCemeteryMemorials").removeClass("uk-button-secondary").addClass("uk-button-primary");
-    setSubmissionFilter("Cemetery");
-  })
+    $('.MemorialFilterButton')
+      .prevAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.MemorialFilterButton')
+      .nextAll()
+      .removeClass('uk-button-primary')
+      .addClass('uk-button-secondary');
+    $('.showCemeteryMemorials').removeClass('uk-button-secondary').addClass('uk-button-primary');
+    setSubmissionFilter('Cemetery');
+  });
 
   function setSubmissionFilter(category) {
-    localStorage.setItem("categoryFilter",category);
-    if (category == "All") {
-      scoredTable.column(5)
-      .search("")
-      .draw()
-      scoredTableData = scoredTable.rows({order:'current', search:'applied'}).data();
+    localStorage.setItem('categoryFilter', category);
+    if (category === 'All') {
+      scoredTable.column(5).search('').draw();
+      scoredTable.rows({ order: 'current', search: 'applied' }).data();
     } else {
-      scoredTable.column(5)
-      .search(category)
-      .draw()
-      scoredTableData = scoredTable.rows({order:'current', search:'applied'}).data();
+      scoredTable.column(5).search(category).draw();
+      scoredTable.rows({ order: 'current', search: 'applied' }).data();
     }
   }
-})
+});
