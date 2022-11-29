17b0d8c5a09cd89e5682be188a115622c48edbae
Tommy Craft <38347315+DJFriar@users.noreply.github.com>
2022-11-29 02:08:13 -0600

    disables gpx route

--- routes/api/index.js
+++ routes/api/index.js
@@ -11,7 +11,7 @@ ApiRouter.use('/bike', require('./v1/bike'));
 ApiRouter.use('/charity', require('./v1/charity'));
 ApiRouter.use('/email', require('./v1/email'));
 ApiRouter.use('/flag', require('./v1/flag'));
-ApiRouter.use('/gpx', require('./v1/gpx'));
+// ApiRouter.use('/gpx', require('./v1/gpx'));
 ApiRouter.use('/memorials', require('./v1/memorials'));
 ApiRouter.use('/orders', require('./v1/orders'));
 ApiRouter.use('/riders', require('./v1/riders'));
