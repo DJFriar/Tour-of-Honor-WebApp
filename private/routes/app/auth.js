da88737d85078e446f884606418e346befc16891
Tommy Craft <38347315+DJFriar@users.noreply.github.com>
2022-12-01 13:45:06 -0600

    update mobile app auth logic

--- private/routes/app/auth.js
+++ private/routes/app/auth.js
@@ -1,30 +1,31 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
+
 const router = express.Router();
-const jwt = require("jsonwebtoken");
-const q = require("../../queries");
+const jwt = require('jsonwebtoken');
+const q = require('../../queries');
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const { flag, zipcode } = req.body;
-  const User = await q.queryUserIDFromFlagNum(flag);
+  // const User = await q.queryUserIDFromFlagNum(flag);
+  const User = await q.queryRiderInfoByFlag(flag);
   const UserData = User[0];
   if (!UserData || UserData.ZipCode !== zipcode) {
-    console.log("==== auth response ====");
-    console.log(User.FlagNumber);
-    return res.status(400).send({ error: "Invalid flag or zipcode." });
+    return res.status(400).send({ error: 'Invalid flag or zipcode.' });
   }
 
   const token = jwt.sign(
-    { UserID: UserData.id, 
-      FirstName: UserData.FirstName, 
-      LastName: UserData.LastName, 
-      Email: UserData.Email, 
+    {
+      UserID: UserData.id,
+      FirstName: UserData.FirstName,
+      LastName: UserData.LastName,
+      Email: UserData.Email,
       FlagNumber: UserData.FlagNumber,
       ZipCode: UserData.ZipCode,
-      PassengerFlag: UserData.PillionFlagNumber
+      PassengerFlag: UserData.PillionFlagNumber,
     },
-    process.env.JWT_SECRET
+    process.env.JWT_SECRET,
   );
   res.send(token);
 });
