d87ffb4ac11010e37f95df0293ae2e57dd1b2318
Tommy Craft <38347315+DJFriar@users.noreply.github.com>
2023-01-18 19:39:29 -0600

    code cleanup

--- static/js/memorial.js
+++ static/js/memorial.js
@@ -1,72 +1,68 @@
-$(document).ready(function() {
-
-  if ($("#MultiImage").val() == "0") {
-    $("#input-primary").bind("change keyup", function () {      
-      if ($("#input-primary").val() != "") {
-        $(this).closest("form").find(":submit").removeAttr("disabled");
-        $("#buttonSpan").removeAttr("uk-tooltip");
+$(document).ready(() => {
+  if ($('#MultiImage').val() === '0') {
+    $('#input-primary').bind('change keyup', function () {
+      if ($('#input-primary').val() !== '') {
+        $(this).closest('form').find(':submit').removeAttr('disabled');
+        $('#buttonSpan').removeAttr('uk-tooltip');
       } else {
-        $(this).closest("form").find(":submit").attr("disabled", "disabled");      
+        $(this).closest('form').find(':submit').attr('disabled', 'disabled');
       }
     });
-    $("#input-optional").prop("required", false);
-  }
-  
-  if ($("#MultiImage").val() == "1") {
-    $("#input-primary").bind("change keyup", function () {      
-      if ($("#input-primary").val() != "" && $("#input-optional").val() != "") {
-        $(this).closest("form").find(":submit").removeAttr("disabled");
-        $("#buttonSpan").removeAttr("uk-tooltip");
-      } else {
-        $(this).closest("form").find(":submit").attr("disabled", "disabled");      
-      }
-    });
-    $("#input-optional").bind("change keyup", function () {      
-      if ($("#input-optional").val() != "" && $("#input-primary").val() != "") {
-        $(this).closest("form").find(":submit").removeAttr("disabled");
-        $("#buttonSpan").removeAttr("uk-tooltip");
-      } else {
-        $(this).closest("form").find(":submit").attr("disabled", "disabled");      
-      }
-    });
+    $('#input-optional').prop('required', false);
   }
 
-  $("#isGroupSubmission").change(function() {
+  if ($('#MultiImage').val() === '1') {
+    $('#input-primary').bind('change keyup', function () {
+      if ($('#input-primary').val() !== '' && $('#input-optional').val() !== '') {
+        $(this).closest('form').find(':submit').removeAttr('disabled');
+        $('#buttonSpan').removeAttr('uk-tooltip');
+      } else {
+        $(this).closest('form').find(':submit').attr('disabled', 'disabled');
+      }
+    });
+    $('#input-optional').bind('change keyup', function () {
+      if ($('#input-optional').val() !== '' && $('#input-primary').val() !== '') {
+        $(this).closest('form').find(':submit').removeAttr('disabled');
+        $('#buttonSpan').removeAttr('uk-tooltip');
+      } else {
+        $(this).closest('form').find(':submit').attr('disabled', 'disabled');
+      }
+    });
+  }
+
+  $('#isGroupSubmission').change(function () {
     if (this.checked) {
-      $("#GroupRiderInfoDiv").removeClass("hide-me")
-      $("#GroupRiderInfo").prop("required", true);
+      $('#GroupRiderInfoDiv').removeClass('hide-me');
+      $('#GroupRiderInfo').prop('required', true);
     } else {
-      $("#GroupRiderInfoDiv").addClass("hide-me")
-      $("#GroupRiderInfo").prop("required", false);
+      $('#GroupRiderInfoDiv').addClass('hide-me');
+      $('#GroupRiderInfo').prop('required', false);
     }
   });
 
-  let handlePrimaryPhoto = function (primaryInput) {
+  const handlePrimaryPhoto = function (primaryInput) {
     if (primaryInput.files) {
-      let reader = new FileReader();
+      const reader = new FileReader();
       reader.onload = function (event) {
-        $(".primaryImagePlaceholder")
-          .attr("src", event.target.result)
+        $('.primaryImagePlaceholder').attr('src', event.target.result);
       };
       reader.readAsDataURL(primaryInput.files[0]);
     }
   };
-  $("#input-primary").on("change", function () {
-    handlePrimaryPhoto(this, "label#inputPrimaryLabel");
+  $('#input-primary').on('change', function () {
+    handlePrimaryPhoto(this, 'label#inputPrimaryLabel');
   });
 
-  let handleOptionalPhoto = function (optionalInput) {
+  const handleOptionalPhoto = function (optionalInput) {
     if (optionalInput.files) {
-      let reader = new FileReader();
+      const reader = new FileReader();
       reader.onload = function (event) {
-        $(".optionalImagePlaceholder")
-          .attr("src", event.target.result)
+        $('.optionalImagePlaceholder').attr('src', event.target.result);
       };
       reader.readAsDataURL(optionalInput.files[0]);
     }
   };
-  $("#input-optional").on("change", function () {
-    handleOptionalPhoto(this, "label#inputOptionalLabel");
+  $('#input-optional').on('change', function () {
+    handleOptionalPhoto(this, 'label#inputOptionalLabel');
   });
-
 });
