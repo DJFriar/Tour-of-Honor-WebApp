aa9d85c0c092bc581442980d1b9d8e59a86a7513
Tommy Craft <38347315+DJFriar@users.noreply.github.com>
2024-09-04 23:24:55 -0500

    Updated scored prior to be 2023 instead of 2022.

--- static/js/scored-prior.js
+++ static/js/scored-prior.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
   // Create the New Scored Table
   $('#HistoricalScoredTable').DataTable({
     ajax: {
-      url: '/api/v1/submission/scored/2022',
+      url: '/api/v1/submission/scored/2023',
       dataSrc: '',
     },
     deferRender: true,
