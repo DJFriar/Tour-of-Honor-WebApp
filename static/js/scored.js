2b10ed9487c64665dffd6af090e419cdab0d127f
Tommy Craft <38347315+DJFriar@users.noreply.github.com>
2025-04-03 01:38:10 -0500

    Corrected the year

--- static/js/scored.js
+++ static/js/scored.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
   // Create the New Scored Table
   $('#scoredTable').DataTable({
     ajax: {
-      url: '/api/v1/submission/scored/2023',
+      url: '/api/v1/submission/scored/2025',
       dataSrc: 'data',
     },
     columns: [
